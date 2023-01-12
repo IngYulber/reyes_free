7eacd2933d91eaa1a2993074e0f7aba269931a0d
IngYulber <johnyulberingalapa@gmail.com>
2023-01-11 21:46:56 -0500

    news detail

---
+++ resources/js/components/News/Detail.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    {{ news }}
+  </div>
+</template>
+
+<script>
+export default {
+    name:"detail",
+    props:{
+        news:{
+            type:Object,
+            required:true
+        }
+    },
+    data(){
+        return {
+
+        }
+    },
+
+    methods:{
+
+    }
+}
+</script>
+
+<style>
+
+</style>
